885fe9e241a84ef72ef442a8c65c6a0b36bcb8b5
maximeguioneau <mguioneau@gmail.com>
2015-06-09 16:13:32 +0200

    Initial Commit

---
+++ Plan_your_events/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Plan_your_events
+//
+//  Created by Maxime Guioneau on 09/06/2015.
+//  Copyright (c) 2015 Maxime Guioneau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Plan_your_eventsTests/Plan_your_eventsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Plan_your_eventsTests.swift
+//  Plan_your_eventsTests
+//
+//  Created by Maxime Guioneau on 09/06/2015.
+//  Copyright (c) 2015 Maxime Guioneau. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Plan_your_eventsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
